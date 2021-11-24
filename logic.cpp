3e98c8179f67584a0ac3cacc772f60741443240b
milescajus <80205080+milescajus@users.noreply.github.com>
2021-11-24 17:07:05 -0600

    implemented random algorithm

--- logic.cpp
+++ logic.cpp
@@ -2,31 +2,125 @@
 #include <cstdlib>
 #include <iostream>
 
-bool **create_board(const int height, const int width) {
-  bool **board = (bool **)malloc(sizeof(bool *) * height);
-  for (int i = 0; i < height; i++) {
-    board[i] = (bool *)malloc(sizeof(bool) * width);
-  }
-  return board;
+using std::to_string;
+
+bool **create_board(const int height, const int width)
+{
+    bool **board = (bool **)malloc(sizeof(bool *) * height);
+    for (int i = 0; i < height; i++) {
+        board[i] = (bool *)malloc(sizeof(bool) * width);
+    }
+    return board;
 }
 
-void destroy_board(bool **board, const int height) {
-  for (int i = 0; i < height; ++i) {
-    free(board[i]);
-  }
-  free(board);
+void destroy_board(bool **board, const int height)
+{
+    for (int i = 0; i < height; ++i) {
+        free(board[i]);
+    }
+    free(board);
 }
 
 void place_ships(bool **board, const int shipcount, const int width,
-                 const int height) {
-  // TODO implement this function
+        const int height)
+{
+    int placed = 0;
+
+    // initialize board
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            board[i][j] = false;
+        }
+    }
+
+    // place ships on free spaces until all are placed
+    while (placed < shipcount) {
+        int a = rand() % height;
+        int b = rand() % width;
+        
+        if (!board[a][b]) {
+            board[a][b] = true;
+            placed++;
+        }
+    }
 }
 
-void find_ships(bool **board, const int width, const int height) {
-  // TODO implement
+Coordinates* find_ships(bool **board, const int width, const int height, const int ship_count)
+{
+    Coordinates *c = (Coordinates *)malloc(sizeof(Coordinates) * ship_count);
+    // Coordinates c[ship_count];
+    int found = 0;
+    int guesses = 0;
+
+    while(found < ship_count) {
+        guesses++;
+
+        int a = rand() % height;
+        int b = rand() % width;
+
+        switch(guess(board, a, b, height, width)) {
+            case ResponseType::HIT:
+                std::cout << "HIT AT (" + to_string(a) + "," + to_string(b) + ")" << std::endl;
+                c[found] = {a, b};      // add coordinates of found ship to array
+                board[a][b] = false;    // prevent ship from being detected twice by removing it
+                found++;
+                break;
+            case ResponseType::MISS:
+                std::cout << "MISS AT (" + to_string(a) + "," + to_string(b) + ")" << std::endl;
+                break;
+            case ResponseType::NEAR_MISS:
+                std::cout << "NEAR MISS AT (" + to_string(a) + "," + to_string(b) + ")" << std::endl;
+                break;
+        }
+    }
+
+    std::cout << to_string(guesses) + " guesses needed" << std::endl;
+
+    for (int i = 0; i < ship_count; i++) {
+        std::cout << c[i].x;
+        std::cout << ":";
+        std::cout << c[i].y << std::endl;
+    }
+
+    return c;
 }
 
-ResponseType guess(bool **board, const int x, const int y) {
-  // TODO implement this
-  return ResponseType::HIT;
+int edge(const int n, const int min, const int max)
+{
+    if (n < min) return min;
+    else if (n > max) return max;
+    else return n;
+}
+
+bool adjacent(bool **board, const int x, const int y, const int height, const int width)
+{
+    int x_lb = edge(x - 1, 0, height - 1);
+    int x_ub = edge(x + 1, 0, height - 1);
+    int y_lb = edge(y - 1, 0, width - 1);
+    int y_ub = edge(y + 1, 0, width - 1);
+
+    return (board[x_lb][y] || board[x_lb][y_ub] || board[x_lb][y_lb] ||
+            board[x][y_lb] || board[x][y_ub] ||
+            board[x_ub][y] || board[x_ub][y_ub] || board[x_ub][y_lb]);
+}
+
+ResponseType guess(bool **board, const int x, const int y, const int height, const int width)
+{
+    if (board[x][y]) {
+        return ResponseType::HIT;
+    } else if (adjacent(board, x, y, height, width)) {
+        return ResponseType::NEAR_MISS;
+    } else {
+        return ResponseType::MISS;
+    }
+}
+
+void print_board(bool **board, const int width, const int height)
+{
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            std::cout << board[i][j];
+        }
+        std::cout << std::endl;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include "logic.h"
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
 int main() {
-  bool **board = create_board(6, 6);
-  destroy_board(board, 6);
+    srand(time(NULL));
+    int height = 6;
+    int width = 6;
+    int ships = 12;
+
+    bool **board = create_board(height, width);
+
+    place_ships(board, ships, height, width);
+    print_board(board, height, width);
+    find_ships(board, height, width, ships);
+
+    destroy_board(board, height);
 }
